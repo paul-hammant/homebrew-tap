7393b3811972cdeaff225168436ff53cdce05aa3
Paul Hammant <paul@hammant.org>
2016-03-13 15:48:45 -0400

    fix sha

--- switchjdk.rb
+++ switchjdk.rb
@@ -2,7 +2,7 @@ class Switchjdk < Formula
   homepage "://github.com/paul-hammant/switchjdk"
   url "https://github.com/paul-hammant/switchjdk/archive/v0.9.6.tar.gz"
   version "0.9.6"
-  sha256 "8999f67c2a5a617d08b15a343afe2b5637f76990b21d05e60808ea189d0958c3"
+  sha256 "a27e4b4b26dc3cbcdb7df0ddf123f4869c20cfd931a2f10d7b85d6bfe3c95a0d"
 
   def install
     bin.install('switchjdk')
