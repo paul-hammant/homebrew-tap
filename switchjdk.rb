91c00a03796a0122a723486ee3ad4ccd773a91d5
Paul Hammant <paul@hammant.org>
2016-03-13 15:47:59 -0400

    bump to 0.9.6 after typo fix by Tim D Smith

--- switchjdk.rb
+++ switchjdk.rb
@@ -1,7 +1,7 @@
 class Switchjdk < Formula
   homepage "://github.com/paul-hammant/switchjdk"
-  url "https://github.com/paul-hammant/switchjdk/archive/v0.9.5.tar.gz"
-  version "0.9.5"
+  url "https://github.com/paul-hammant/switchjdk/archive/v0.9.6.tar.gz"
+  version "0.9.6"
   sha256 "8999f67c2a5a617d08b15a343afe2b5637f76990b21d05e60808ea189d0958c3"
 
   def install
