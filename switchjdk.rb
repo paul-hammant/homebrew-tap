0385533a85865c9dedb9f25742e359aa45c491ee
Paul Hammant <paul@hammant.org>
2016-03-18 04:09:55 -0400

    back to bash function so as to make exports sticky

--- switchjdk.rb
+++ switchjdk.rb
@@ -1,11 +1,20 @@
 class Switchjdk < Formula
   homepage "://github.com/paul-hammant/switchjdk"
-  url "https://github.com/paul-hammant/switchjdk/archive/v0.9.6.tar.gz"
-  version "0.9.6"
-  sha256 "a27e4b4b26dc3cbcdb7df0ddf123f4869c20cfd931a2f10d7b85d6bfe3c95a0d"
+  url "https://github.com/paul-hammant/switchjdk/archive/v0.9.7.tar.gz"
+  version "0.9.7"
+  sha256 "ab3d81cf05c3dbd2487aae8a259b631e24eb1bdced00ae5b336397a67b4ba2ed"
 
   def install
-    bin.install('switchjdk')
+      etc.install 'switchjdk-module.bash'
   end
+
+  def caveats; <<-EOS.undent
+      Add the following lines to your ~/.bash_profile:
+        if [ -f $(brew --prefix)/etc/switchjdk-module.bash ]; then
+          source $(brew --prefix)/etc/switchjdk-module.bash
+        fi
+      Start a new terminal session or source ~/.bash_profile to be able to use switchjdk.
+      EOS
+    end
 
 end
