56618b23b048f64c8137de9d08ace19b47221a90
Paul Hammant <paul@hammant.org>
2016-08-07 11:49:11 -0400

    1.0.1 of slim

--- git-slim.rb
+++ git-slim.rb
@@ -1,8 +1,8 @@
 class GitSlim < Formula
   homepage "://github.com/paul-hammant/git-slim"
-  url "https://github.com/paul-hammant/git-slim/archive/v1.0.tar.gz"
+  url "https://github.com/paul-hammant/git-slim/archive/v1.0.1.tar.gz"
   version "1.0"
-  sha256 "478106565647f6639e5e06d808a8d4ef560f16babca07139d25a1791e10d4899"
+  sha256 "c16761a54ba007e97e20680b308acf49fb99051cac3b7039c86647eec8658947"
 
   def install
       bin.install 'git-slim'
