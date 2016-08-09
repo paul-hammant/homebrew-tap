9ee5b765101cb3d794f3a13894faa8a74fab7ec2
Paul Hammant <paul@hammant.org>
2016-08-09 07:10:57 -0400

    v1.0.2

--- git-size.rb
+++ git-size.rb
@@ -1,8 +1,8 @@
 class GitSize < Formula
   homepage "://github.com/paul-hammant/git-size"
-  url "https://github.com/paul-hammant/git-size/archive/v1.0.1.tar.gz"
-  version "1.0.1"
-  sha256 "6614d1303a5ebff4c873d0f679c979b3f5e8c61b5e78626c5d1fed1c8c53f858"
+  url "https://github.com/paul-hammant/git-size/archive/v1.0.2.tar.gz"
+  version "1.0.2"
+  sha256 "92025d9b039444a7d0949d42dc5927c89a4e1bafcccd8207751b5bec80ac52e9"
 
   def install
       bin.install 'git-size'
