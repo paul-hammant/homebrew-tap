967c18379df7c7885fd4e0af35fefc897e1e951f
Paul Hammant <paul@hammant.org>
2016-08-14 14:29:03 -0400

    base64 dep

--- git-size.rb
+++ git-size.rb
@@ -1,4 +1,5 @@
 class GitSize < Formula
+  depends_on "base64"
   homepage "://github.com/paul-hammant/git-size"
   url "https://github.com/paul-hammant/git-size/archive/v1.0.5.tar.gz"
   version "1.0.5"
