980ae21486126a7a8b838f5f949cd9b629e925e4
Paul Hammant <paul@hammant.org>
2018-03-22 20:05:56 -0400

    JDK 11 EA too

--- switchjdk.rb
+++ switchjdk.rb
@@ -1,8 +1,8 @@
 class Switchjdk < Formula
   homepage "://github.com/paul-hammant/switchjdk"
-  url "https://github.com/paul-hammant/switchjdk/archive/v1.1.tar.gz"
+  url "https://github.com/paul-hammant/switchjdk/archive/v1.1.1.tar.gz"
   version "1.1"
-  sha256 "e66b143f46b7f48d6418d06b8a81b79ce12d7b24d89f54da7fb606619350d7ef"
+  sha256 "6453616e2592d139023b8c96464ce81383ee3a1faf3c79fb5bc6e694dfff2c52"
 
   # shasum -a 256 v1.1.tar.gz
 
@@ -18,7 +18,7 @@ class Switchjdk < Formula
       system "bash", "add_switchjdk_to_bash_profile_safely.sh"
   end
 
-  def caveats; <<-EOS.undent
+  def caveats; <<-EOS
       Your ~/.bash_profile had a couple of lines added to it to make switchjdk available on the command line.
       They will still be there after an uninstall, but are adaptive (nothing happens if switchjdk was uninstalled)
       If you're a zsh person, then patches are welcome: https://github.com/paul-hammant/switchjdk/blob/master/add_switchjdk_to_bash_profile_safely.sh
